13c5d0cc646e2a40fe5cf5aa9d3d9f8b76accb68
Jade <taoyulong@gmail.com>
2024-03-16 15:48:49 +0900

    Add .gitignore, Cargo.toml, and src/lib.rs files

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+use chrono::Local;
+use env_logger::Builder as LoggerBuilder;
+use std::io::Write;
+
+pub fn setup_logger() {
+    LoggerBuilder::new()
+        .format(|buf, record| {
+            let level_style = buf.default_level_style(record.level());
+            let reset = level_style.render_reset();
+            let level_style = level_style.render();
+
+            // let error_style = buf.default_level_style(log::Level::Error);
+            // let error_reset = error_style.render_reset();
+            // let error_style = error_style.render();
+
+            let now = Local::now();
+
+            let message = format!("{}", record.args());
+
+            if message.trim().is_empty() {
+                // 如果消息体为空，则输出一个空行或自定义的空消息格式
+                writeln!(buf)
+            } else {
+                // 否则，按常规方式输出日志消息
+                writeln!(
+                    buf,
+                    "{} {} {level_style}{}{reset}  - {}",
+                    now.format("%Y-%m-%d"),
+                    now.format("%H:%M:%S"),
+                    format!("[{}]", record.level()),
+                    message
+                )
+            }
+        })
+        .filter_level(log::LevelFilter::Info)
+        .init(); // ...
+}
